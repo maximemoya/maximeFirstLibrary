069821afc8e00f37cc6e4442394ec4f703bb4df3
Maxime <maxime#email.com>
2023-12-13 08:26:47 +0100

    date improve lib version 1.1.0

--- README.md
+++ README.md
@@ -10,14 +10,100 @@ ref: https://www.youtube.com/watch?v=Nh9xW2-ZOEU
 # How to use :
 ```
 import { MaximeSerialize } from 'maximefirstlibrary'
+
 MaximeSerialize.writeFileSync('./a/b/c.txt', 'test')
 console.log(MaximeSerialize.readFileSync('./a/b/c.txt'));
 
+---------------------------------------------------------
+
 import { MaximeTime } from 'maximefirstlibrary'
-console.log(MaximeTime.getActualLocalDateTimeISO())
-console.log(MaximeTime.getActualLocalDateTime())
-console.log(MaximeTime.getActualLocalDate())
-console.log(MaximeTime.getActualLocalTime())
+
 console.log(MaximeTime.getActualDateTimeISO())
 console.log(MaximeTime.getActualDateTimeUTC())
+console.log(MaximeTime.getActualLocalDate())
+console.log(MaximeTime.getActualLocalDateTimeISO())
+console.log(MaximeTime.getActualLocalDateTimeObject())
+console.log(MaximeTime.getActualLocalTime24())
+
+const date1 = new Date(1900, 1, 1, 1, 1, 1)
+console.log(MaximeTime.getLocalDateTimeObjectFrom(date1))
+
+const date2 = new Date(1900, 1, 1, 1, 1, 2)
+console.log(MaximeTime.getLocalDateTimeObjectFrom(date2))
+
+console.log('is date1 before date2 according year ? ', MaximeTime.isDateBefore(date1, date2, "year"))
+console.log('is date1 before date2 according month ? ', MaximeTime.isDateBefore(date1, date2, "month"))
+console.log('is date1 before date2 according day ? ', MaximeTime.isDateBefore(date1, date2, "day"))
+console.log('is date1 before date2 according hour ? ', MaximeTime.isDateBefore(date1, date2, "hour"))
+console.log('is date1 before date2 according minute ? ', MaximeTime.isDateBefore(date1, date2, "minute"))
+console.log('is date1 before date2 according second ? ', MaximeTime.isDateBefore(date1, date2, "second"))
+
+console.log('is date2 after date1 according year ? ', MaximeTime.isDateAfter(date2, date1, "year"))
+console.log('is date2 after date1 according month ? ', MaximeTime.isDateAfter(date2, date1, "month"))
+console.log('is date2 after date1 according day ? ', MaximeTime.isDateAfter(date2, date1, "day"))
+console.log('is date2 after date1 according hour ? ', MaximeTime.isDateAfter(date2, date1, "hour"))
+console.log('is date2 after date1 according minute ? ', MaximeTime.isDateAfter(date2, date1, "minute"))
+console.log('is date2 after date1 according second ? ', MaximeTime.isDateAfter(date2, date1, "second"))
+
+console.log('is date2 equal date1 according year ? ', MaximeTime.isDateEqual(date2, date1, "year"))
+console.log('is date2 equal date1 according month ? ', MaximeTime.isDateEqual(date2, date1, "month"))
+console.log('is date2 equal date1 according day ? ', MaximeTime.isDateEqual(date2, date1, "day"))
+console.log('is date2 equal date1 according hour ? ', MaximeTime.isDateEqual(date2, date1, "hour"))
+console.log('is date2 equal date1 according minute ? ', MaximeTime.isDateEqual(date2, date1, "minute"))
+console.log('is date2 equal date1 according second ? ', MaximeTime.isDateEqual(date2, date1, "second"))
 ```
+
+## package.json to make a lib
+```
+{
+  "name": "yourLibraryName",
+  "version": "1.0.0",
+  "description": "your first library",
+  "main": "dist/index.js",
+  "types": "dist/index.d.ts",
+  "files": [
+    "dist"
+  ],
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/yourGithubName/yourGithubLibrary.git"
+  },
+  "keywords": [
+    "your-lib-keywords"
+  ]
+}
+```
+
+## tsconfig.json to make a lib
+```
+{
+  "compilerOptions": {
+    /* Visit https://aka.ms/tsconfig to read more about this file */
+
+    /* Language and Environment */
+    "target": "es2016",                                  /* Set the JavaScript language version for emitted JavaScript and include compatible library declarations. */
+
+    /* Modules */
+    "module": "commonjs",                                /* Specify what module code is generated. */
+    
+    /* Emit */
+    "declaration": true,                                 /* Generate .d.ts files from TypeScript and JavaScript files in your project. */
+    "sourceMap": true,                                   /* Create source map files for emitted JavaScript files. */
+    "outDir": "./dist",                                  /* Specify an output folder for all emitted files. */
+
+    /* Interop Constraints */
+    "esModuleInterop": true,                             /* Emit additional JavaScript to ease support for importing CommonJS modules. This enables 'allowSyntheticDefaultImports' for type compatibility. */
+    "forceConsistentCasingInFileNames": true,            /* Ensure that casing is correct in imports. */
+
+    /* Type Checking */
+    "strict": true,                                      /* Enable all strict type-checking options. */
+    "noImplicitAny": true,                               /* Enable error reporting for expressions and declarations with an implied 'any' type. */
+    
+    /* Completeness */
+    "skipLibCheck": true                                 /* Skip type checking all .d.ts files. */
+  },
+  "exclude": ["node_modules", "dist"],
+  "include": ["src/**/*"]
+}
+```
+
--- src/mylib/MaximeTime.ts
+++ src/mylib/MaximeTime.ts
@@ -1,15 +1,8 @@
 const date = new Date()
 
-export function getActualLocalDateTimeISO() {
-    return date.getFullYear()
-        + '-' + (date.getMonth() + 1).toString().padStart(2, '0')
-        + '-' + date.getDate().toString().padStart(2, '0')
-        + 'T' + date.getHours().toString().padStart(2, '0')
-        + ':' + date.getMinutes().toString().padStart(2, '0')
-        + ':' + date.getSeconds().toString().padStart(2, '0')
-        + '.' + date.getMilliseconds().toString().padStart(3, '0')
-        + 'L'
-}
+// -------
+//  Lima
+// -------
 
 export interface MyDateTime {
     year: number,
@@ -35,7 +28,74 @@ export function getActualLocalDateTimeObject(): MyDateTime {
     }
 }
 
+export function getLocalDateTimeObjectFrom(date: Date): MyDateTime {
+    return {
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        dayOfTheMonth: date.getDate(),
+        dayOfTheWeek: date.getDay(),
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds(),
+        millisecond: date.getMilliseconds()
+    }
+}
+
+export function getActualLocalDateTimeISO() {
+    return date.getFullYear()
+        + '-' + (date.getMonth() + 1).toString().padStart(2, '0')
+        + '-' + date.getDate().toString().padStart(2, '0')
+        + 'T' + date.getHours().toString().padStart(2, '0')
+        + ':' + date.getMinutes().toString().padStart(2, '0')
+        + ':' + date.getSeconds().toString().padStart(2, '0')
+        + '.' + date.getMilliseconds().toString().padStart(3, '0')
+        + 'L'
+}
+
+export function getLocalDateTimeISOFrom(date: Date) {
+    return date.getFullYear()
+        + '-' + (date.getMonth() + 1).toString().padStart(2, '0')
+        + '-' + date.getDate().toString().padStart(2, '0')
+        + 'T' + date.getHours().toString().padStart(2, '0')
+        + ':' + date.getMinutes().toString().padStart(2, '0')
+        + ':' + date.getSeconds().toString().padStart(2, '0')
+        + '.' + date.getMilliseconds().toString().padStart(3, '0')
+        + 'L'
+}
+
+export function getActualLocalDate() {
+    return date.getFullYear()
+        + '-' + (date.getMonth() + 1).toString().padStart(2, '0')
+        + '-' + date.getDate().toString().padStart(2, '0')
+}
+
+export function getLocalDateFrom(date: Date) {
+    return date.getFullYear()
+        + '-' + (date.getMonth() + 1).toString().padStart(2, '0')
+        + '-' + date.getDate().toString().padStart(2, '0')
+}
+
+export function getActualLocalTime24() {
+    return date.getHours().toString().padStart(2, '0')
+        + ':' + date.getMinutes().toString().padStart(2, '0')
+        + ':' + date.getSeconds().toString().padStart(2, '0')
+}
+
+export function getLocalTime24From(date: Date) {
+    return date.getHours().toString().padStart(2, '0')
+        + ':' + date.getMinutes().toString().padStart(2, '0')
+        + ':' + date.getSeconds().toString().padStart(2, '0')
+}
+
+// --------
+//  Zoulou
+// --------
+
 export function getActualDateTimeISO() {
+    return date.toISOString()
+}
+
+export function getDateTimeISOFrom(date: Date) {
     return date.toISOString()
 }
 
@@ -43,14 +103,336 @@ export function getActualDateTimeUTC() {
     return date.toUTCString()
 }
 
-export function getActualLocalDateTime() {
-    return date.toLocaleString()
+export function getDateTimeUTCFrom(date: Date) {
+    return date.toUTCString()
 }
 
-export function getActualLocalDate() {
-    return date.toLocaleDateString()
+// -------------
+// CONDITIONNAL
+// -------------
+
+export function isDateOnlySameYear(dateA: Date, dateB: Date) {
+    return (dateA.getFullYear() === dateB.getFullYear())
 }
 
-export function getActualLocalTime() {
-    return date.toLocaleTimeString()
+export function isDateOnlySameMonth(dateA: Date, dateB: Date) {
+    return (dateA.getMonth() === dateB.getMonth())
+}
+
+export function isDateOnlySameDay(dateA: Date, dateB: Date) {
+    return (dateA.getDate() === dateB.getDate())
+}
+
+export function isDateOnlySameHour(dateA: Date, dateB: Date) {
+    return (dateA.getHours() === dateB.getHours())
+}
+
+export function isDateOnlySameMinute(dateA: Date, dateB: Date) {
+    return (dateA.getMinutes() === dateB.getMinutes())
+}
+
+export function isDateOnlySameSecond(dateA: Date, dateB: Date) {
+    return (dateA.getSeconds() === dateB.getSeconds())
+}
+
+// ---------------------------
+
+export function isDateEqual(
+    dateA: Date,
+    dateB: Date,
+    according: "year" | "month" | "day" | "hour" | "minute" | "second" | undefined = undefined
+) {
+    switch (according) {
+        case undefined: {
+            return (dateA.getTime() === dateB.getTime())
+        }
+        case "year": {
+            return (dateA.getFullYear() === dateB.getFullYear())
+        }
+        case "month": {
+            return (
+                dateA.getFullYear() === dateB.getFullYear() &&
+                dateA.getMonth() === dateB.getMonth()
+            )
+        }
+        case "day": {
+            return (
+                dateA.getFullYear() === dateB.getFullYear() &&
+                dateA.getMonth() === dateB.getMonth() &&
+                dateA.getDay() === dateB.getDay()
+            )
+        }
+        case "hour": {
+            return (
+                dateA.getFullYear() === dateB.getFullYear() &&
+                dateA.getMonth() === dateB.getMonth() &&
+                dateA.getDay() === dateB.getDay() &&
+                dateA.getHours() === dateB.getHours()
+            )
+        }
+        case "minute": {
+            return (
+                dateA.getFullYear() === dateB.getFullYear() &&
+                dateA.getMonth() === dateB.getMonth() &&
+                dateA.getDay() === dateB.getDay() &&
+                dateA.getHours() === dateB.getHours() &&
+                dateA.getMinutes() === dateB.getMinutes()
+            )
+        }
+        case "second": {
+            return (
+                dateA.getFullYear() === dateB.getFullYear() &&
+                dateA.getMonth() === dateB.getMonth() &&
+                dateA.getDay() === dateB.getDay() &&
+                dateA.getHours() === dateB.getHours() &&
+                dateA.getMinutes() === dateB.getMinutes() &&
+                dateA.getSeconds() === dateB.getSeconds()
+            )
+        }
+        default: throw new Error("unhandle case")
+    }
+}
+
+export function isDateBefore(
+    dateA: Date,
+    dateB: Date,
+    according: "year" | "month" | "day" | "hour" | "minute" | "second" | undefined = undefined
+) {
+    switch (according) {
+        case undefined: {
+            return (dateA.getTime() < dateB.getTime());
+        }
+        case "year": {
+            return (dateA.getFullYear() < dateB.getFullYear());
+        }
+        case "month": {
+
+            return (
+                dateA.getFullYear() < dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() < dateB.getMonth()
+                )
+            );
+        }
+        case "day": {
+            return (
+                dateA.getFullYear() < dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() < dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() < dateB.getDate()
+                )
+            );
+        }
+        case "hour": {
+            return (
+                dateA.getFullYear() < dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() < dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() < dateB.getDate()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() < dateB.getHours()
+                )
+            );
+        }
+        case "minute": {
+            return (
+                dateA.getFullYear() < dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() < dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() < dateB.getDate()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() < dateB.getHours()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() === dateB.getHours() &&
+                    dateA.getMinutes() < dateB.getMinutes()
+                )
+            );
+        }
+        case "second": {
+            return (
+                dateA.getFullYear() < dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() < dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() < dateB.getDate()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() < dateB.getHours()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() === dateB.getHours() &&
+                    dateA.getMinutes() < dateB.getMinutes()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() === dateB.getHours() &&
+                    dateA.getMinutes() === dateB.getMinutes() &&
+                    dateA.getSeconds() < dateB.getSeconds()
+                )
+            );
+        }
+        default: throw new Error("unhandle case")
+    }
+}
+
+export function isDateAfter(
+    dateA: Date,
+    dateB: Date,
+    according: "year" | "month" | "day" | "hour" | "minute" | "second" | undefined = undefined
+) {
+    switch (according) {
+        case undefined: {
+            return (dateA.getTime() > dateB.getTime());
+        }
+        case "year": {
+            return (dateA.getFullYear() > dateB.getFullYear());
+        }
+        case "month": {
+
+            return (
+                dateA.getFullYear() > dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() > dateB.getMonth()
+                )
+            );
+        }
+        case "day": {
+            return (
+                dateA.getFullYear() > dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() > dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() > dateB.getDate()
+                )
+            );
+        }
+        case "hour": {
+            return (
+                dateA.getFullYear() > dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() > dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() > dateB.getDate()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() > dateB.getHours()
+                )
+            );
+        }
+        case "minute": {
+            return (
+                dateA.getFullYear() > dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() > dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() > dateB.getDate()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() > dateB.getHours()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() === dateB.getHours() &&
+                    dateA.getMinutes() > dateB.getMinutes()
+                )
+            );
+        }
+        case "second": {
+            return (
+                dateA.getFullYear() > dateB.getFullYear() ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() > dateB.getMonth()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() > dateB.getDate()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() > dateB.getHours()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() === dateB.getHours() &&
+                    dateA.getMinutes() > dateB.getMinutes()
+                ) ||
+                (
+                    dateA.getFullYear() === dateB.getFullYear() &&
+                    dateA.getMonth() === dateB.getMonth() &&
+                    dateA.getDate() === dateB.getDate() &&
+                    dateA.getHours() === dateB.getHours() &&
+                    dateA.getMinutes() === dateB.getMinutes() &&
+                    dateA.getSeconds() > dateB.getSeconds()
+                )
+            );
+        }
+        default: throw new Error("unhandle case")
+    }
 }
